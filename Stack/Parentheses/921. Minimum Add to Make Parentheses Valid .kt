730ec37e40c54c1e460e5ff698b522b491c351e8
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-05-20 09:34:50 +0530

    Update 921. Minimum Add to Make Parentheses Valid .kt

--- Stack/Parentheses/921. Minimum Add to Make Parentheses Valid .kt
+++ Stack/Parentheses/921. Minimum Add to Make Parentheses Valid .kt
@@ -1,1 +1,21 @@
 
+ fun minAddToMakeValid(s: String): Int {
+
+        var countOpen:Int = 0;
+        var countClose:Int = 0;
+
+        s.forEach{parentheses ->
+            if(parentheses == '('){
+                    countOpen++;
+            }
+           else if(countOpen>0){
+                    countOpen--;
+            }
+                else{               // for '))))' these case not '()' case
+                    countClose++;
+                }
+            }
+
+            return countOpen+countClose;
+
+    }  
