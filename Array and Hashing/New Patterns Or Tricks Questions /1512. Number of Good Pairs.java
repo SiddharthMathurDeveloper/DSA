324b9f3bb7519e58a74fbb09a338636338594ae1
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-27 13:25:08 +0530

    Update 1512. Number of Good Pairs.java

--- Array and Hashing/New Patterns Or Tricks Questions /1512. Number of Good Pairs.java
+++ Array and Hashing/New Patterns Or Tricks Questions /1512. Number of Good Pairs.java
@@ -1,27 +1,35 @@
 
 
+/* 
+  Brute Force & Optimized -> Time - O(N) Space - O(N) 
+  
+*/ 
+
 
 
 class Solution {
     public int numIdenticalPairs(int[] nums) {
 
-        HashMap<Integer,Integer> hashMap = new HashMap<>();
-        int sumOfPairs=0;
+        HashMap<Integer,Integer> hashMap = new HashMap<>(); //store freq's in hashmap later will need them
+        
+        int sumOfPairs=0; // sum of pairs foromed
 
-        for (Integer number: nums) {
+        for (Integer number: nums) { // loop and get freq of each element
+            
             hashMap.put(number, hashMap.getOrDefault(number, 0) + 1);
 
         }
 
 
-        for (Integer key : hashMap.keySet()) {
-            Integer n = hashMap.get(key);
+        for (Integer key : hashMap.keySet()) { // loop over the hashmap 
+            
+            Integer n = hashMap.get(key); //Get the freq of element 
 
-            sumOfPairs+= (n*(n-1))/2;
+            sumOfPairs+= (n*(n-1))/2; // use sum of n number formula((n*(n-1))/2) eg [1,1,1,1] for 1-> 4 sum is -> 6  add that where n is freq
 
         }
 
-        return sumOfPairs;
+        return sumOfPairs; // return sum
        
         
         
