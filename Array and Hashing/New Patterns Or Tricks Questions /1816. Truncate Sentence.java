7970e065455e7b20210a5f3148fbc129f0531f25
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-27 18:34:43 +0530

    Update 1816. Truncate Sentence.java

--- Array and Hashing/New Patterns Or Tricks Questions /1816. Truncate Sentence.java
+++ Array and Hashing/New Patterns Or Tricks Questions /1816. Truncate Sentence.java
@@ -1,5 +1,15 @@
 
 
+/* 
+
+  Brute Force & Optimized -> Time - O(N) Space - O(1) 
+  
+   TAG -> [String]
+
+   Trick -> Get the freq of elements put in hashmap these freq will n then to get the sum put in formula of [sum of nth number of same kind((n*(n-1))/2)]
+  
+*/ 
+
 
 class Solution {
     public String truncateSentence(String s, int k) {
