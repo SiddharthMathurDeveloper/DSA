6dc02850b001910ecf1b73ca847ef3ee983baff9
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-27 14:02:12 +0530

    Update 1431. Kids With the Greatest Number of Candies.java

--- Array and Hashing/All Questions/1431. Kids With the Greatest Number of Candies.java
+++ Array and Hashing/All Questions/1431. Kids With the Greatest Number of Candies.java
@@ -1,30 +1,45 @@
 
 
 
+/* 
+  Brute Force & Optimized -> Time - O(N) Space - O(1) 
+  
+*/ 
+
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int maxi = Integer.MIN_VALUE;
-        List<Boolean> boolCandies = new ArrayList<Boolean>();
+        
+        int maxi = Integer.MIN_VALUE; // make a maxi variable to store the max value to compare in later stage
+        
+        List<Boolean> boolCandies = new ArrayList<Boolean>(); // To store the answer
 
-        for(int candide:candies){
-            maxi = Math.max(maxi,candide);
+        for(int candide:candies){ // loop over the candies to get the max value
+            
+            maxi = Math.max(maxi,candide); // store max value in this maxi variable
+            
         }
 
-        int index=0;
-        for(int candide:candies){
+        int index=0; // index for using for each loop
+        
+        for(int candide:candies){ // Loop over the candies
+            
             if(candide+extraCandies>=maxi){
+                
                 boolCandies.add(true);
+                
             }
+                
             else{
+                
                 boolCandies.add(false);
+                
             }
         }
 
 
         return boolCandies;
 
-
-        
 
     }
 }
