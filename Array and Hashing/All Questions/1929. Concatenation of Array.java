ae89fef1074650560349d4957f6f7f3f0e98a98d
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-27 10:38:22 +0530

    Update 1929. Concatenation of Array.java

--- Array and Hashing/All Questions/1929. Concatenation of Array.java
+++ Array and Hashing/All Questions/1929. Concatenation of Array.java
@@ -6,6 +6,7 @@
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
+      
          int numsLength = nums.length; // get the length of nums
 
         int [] concatedNums = new int[numsLength*2]; // store the result double of n [0,0,0,0,0,0]
@@ -15,9 +16,10 @@ class Solution {
          concatedNums[i] = nums[i]; // get first value store ith index [1,0,0,0,0,0]  [1,2,1]
                
          concatedNums[i+numsLength] = nums[i]; // get first value store in n+1 index [1,0,0,1,0,0]  [1,2,1]] i=0 n=3 i+n = 3 
+          
         }
 
-        
         return  concatedNums;
+      
     }
 }
