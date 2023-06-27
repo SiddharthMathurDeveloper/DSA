4aa97d2953023a135de41709ada97b10758b1247
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-28 00:08:18 +0530

    Create 771. Jewels and Stones.java

---
+++ Array and Hashing/All Questions/771. Jewels and Stones.java
@@ -0,0 +1,24 @@
+
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+      
+        Map<Character,Integer>freq_map = new HashMap<Character,Integer>();
+
+        int distinctJewels=0;
+        for(char stone:stones.toCharArray()){
+
+            freq_map.put(stone,freq_map.getOrDefault(stone,0)+1);
+
+        }  
+
+        for(char jewel:jewels.toCharArray()){
+            if(freq_map.containsKey(jewel)){
+                distinctJewels+=freq_map.get(jewel);
+            }
+        }
+
+
+        return distinctJewels;
+    }
+}
