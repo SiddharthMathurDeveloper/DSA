5af1e524dc09bec7b560f65bb56326d4538b37cc
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-27 14:38:40 +0530

    Update 1480. Running Sum of 1d Array.java

--- Array and Hashing/All Questions/1480. Running Sum of 1d Array.java
+++ Array and Hashing/All Questions/1480. Running Sum of 1d Array.java
@@ -1,13 +1,23 @@
 
+/* 
+
+  Brute Force & Optimized -> Time - O(N) Space - O(1) 
+  
+*/ 
+
+
 class Solution {
     public int[] runningSum(int[] nums) {
+        
         int sum=0;
-
-
+        
         int index=0;
         for(int num:nums){
+            
             sum+=num;
+            
             nums[index] = sum;
+            
             index++;
         }
 
