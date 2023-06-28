7ca4f6d0faab77c9977a5937c61d5ab0bbcbad5c
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-28 13:58:25 +0530

    Create 1221. Split a String in Balanced Strings.java

---
+++ Array and Hashing/New Patterns Or Tricks Questions /1221. Split a String in Balanced Strings.java
@@ -0,0 +1,29 @@
+
+
+class Solution {
+    public int balancedStringSplit(String s) {
+        int countRight=0;
+        int countLeft=0;
+
+        int maximumBalanceStrings=0;
+
+        for(char value:s.toCharArray()){
+
+            if(value=='R'){
+                countRight++;
+            }
+
+            if(value=='L'){
+                countLeft++;
+            }
+
+            if(countRight==countLeft){
+                maximumBalanceStrings+=1;
+                countRight=0;
+                countLeft=0;
+            }
+        } 
+
+        return  maximumBalanceStrings;
+    }
+}
