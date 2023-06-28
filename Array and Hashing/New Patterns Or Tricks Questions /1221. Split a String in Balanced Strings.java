eeb19ca9b7e5b61d9653754159cc93b532068cb8
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-28 15:36:36 +0530

    Update 1221. Split a String in Balanced Strings.java

--- Array and Hashing/New Patterns Or Tricks Questions /1221. Split a String in Balanced Strings.java
+++ Array and Hashing/New Patterns Or Tricks Questions /1221. Split a String in Balanced Strings.java
@@ -6,32 +6,38 @@
   
    TAG -> [String , Letter counter , conditions ]
 
-   Trick -> 
+   Trick -> If you observe the pattern of getting substrings then u will notice every pattern as equal R and L , So from this observesation I can say that when R==L the count then we have found a Valid String 
   
 */ 
 
 
+// eg "RRLLRL" ans-> 2 observe the first substring RRLL R2==L2 the second R1==L1 so everytime when both becomes equal we have found it 
 
 class Solution {
     public int balancedStringSplit(String s) {
-        int countRight=0;
-        int countLeft=0;
+        int countRight=0; // To count Right R
+        int countLeft=0; // To count Right L
 
-        int maximumBalanceStrings=0;
+        int maximumBalanceStrings=0; // The result
 
-        for(char value:s.toCharArray()){
+        for(char value:s.toCharArray()){ // loop over the given string
 
-            if(value=='R'){
+            if(value=='R'){ // If R countRight ++
+              
                 countRight++;
             }
 
-            if(value=='L'){
+            if(value=='L'){ // If L countLeft ++
+              
                 countLeft++;
             }
 
-            if(countRight==countLeft){
-                maximumBalanceStrings+=1;
-                countRight=0;
+            if(countRight==countLeft){ // When Equal we have found the string 
+              
+                maximumBalanceStrings+=1; // increase the result by one 
+              
+                countRight=0; // make them zero as new are in search of new string 
+              
                 countLeft=0;
             }
         } 
