5a4255c30dee3d4dc17fde56a274f6560920e188
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-06-29 10:02:00 +0530

    Update and rename q.java to 2697. Lexicographically Smallest Palindrome.java

---
+++ Two Pointers/All Questions/2697. Lexicographically Smallest Palindrome.java
@@ -0,0 +1,40 @@
+
+
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        
+        int i = 0; // Start this pointer from start
+
+        int j= s.length()-1; // Start this pointer from end
+
+        char[] charArr = s.toCharArray(); // convert to char Array to overwrite values
+
+        while(i<=j){
+
+            char startIndex = s.charAt(i); // Get character at ith element
+            
+            char endIndex = s.charAt(j); // Get character at jth element
+            
+            /* Get Min char and overwrite the greater value that index eg . [e,g,c,f,e] -> [e,f,c,f,e] for f,g replace f->g or g<-f any value it will be one step toward being palidrome but question   also asked us to make lexiograhy which can be done using can get min char of the two */
+              
+          
+            if(startIndex>endIndex){
+                charArr[i] = endIndex;
+            }
+            else if(startIndex<endIndex){
+                charArr[j] = startIndex;
+            }
+
+            i++; // move i fowards
+            j--; // move j backwards
+        }
+
+
+       
+        return String.valueOf(charArr); // convert to string and return the ans
+
+        
+
+
+    }
+}
