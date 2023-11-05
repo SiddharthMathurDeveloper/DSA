1651a206e310bfdfb9e77bc6e3474ee597df4b3f
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-11-05 11:07:05 +0530

    Update 1572. Matrix Diagonal Sum.java

--- Matrix/Questions /1572. Matrix Diagonal Sum.java
+++ Matrix/Questions /1572. Matrix Diagonal Sum.java
@@ -1,25 +1,34 @@
 ```java
 
-
-  class Solution {
+class Solution {
     public int diagonalSum(int[][] mat) {
-        int n = mat.length;
+    // Get the size of the square matrix
+    int n = mat.length;
 
-        int sumOfBothDiagonal = 0;
-        for(int i=0;i<n;i++){
-            sumOfBothDiagonal+=mat[i][i]; // primary diagonal 
-            sumOfBothDiagonal+=mat[i][n-i-1]; // Secondary diagonal
-        }
+    // Initialize a variable to store the sum of both diagonals
+    int sumOfBothDiagonals = 0;
 
-        if(n%2!=0){
-          int midElement = mat[n/2][n/2];
+    // Loop through the rows and columns of the matrix
+    for (int i = 0; i < n; i++) {
+        // Add the element from the primary diagonal to the sum
+        sumOfBothDiagonals += mat[i][i]; // Primary diagonal
 
-          sumOfBothDiagonal-=midElement; // subract the middle 
-
-        }
-
-        return sumOfBothDiagonal;
+        // Add the element from the secondary diagonal to the sum
+        sumOfBothDiagonals += mat[i][n - i - 1]; // Secondary diagonal
     }
+
+    // Check if the matrix has an odd dimension (center element exists)
+    if (n % 2 != 0) {
+        // Get the element at the center of the matrix
+        int midElement = mat[n / 2][n / 2];
+
+        // Subtract the middle element from the sum (as it was added twice)
+        sumOfBothDiagonals -= midElement;
+    }
+
+    // Return the sum of both diagonals
+    return sumOfBothDiagonals;
+}
 }
 
 
