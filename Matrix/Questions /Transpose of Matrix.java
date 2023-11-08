c903738cbd9c0eecfd33931a47f8fdaed59b5d38
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-11-08 09:17:05 +0530

    Create Transpose of Matrix.java

---
+++ Matrix/Questions /Transpose of Matrix.java
@@ -0,0 +1,17 @@
+class Solution{
+public void transpose(int n, int matrix[][]) {
+    int matrixRow = matrix.length; // Get the number of rows in the matrix
+    int matrixCol = matrix[0].length; // Get the number of columns in the matrix
+
+    // Iterate through the rows and columns of the matrix
+    for (int i = 0; i < matrixRow; i++) {
+        for (int j = 0; j < i; j++) {
+            // Swap the elements at position (i, j) and (j, i) to transpose the matrix
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
+
+}
