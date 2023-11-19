840a9720694a232cbbeb07aa2edc64eeeb3a9c68
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-11-20 01:04:47 +0530

    Update Subset Sums.java

--- Backtracking/Questions/Subset Sums.java
+++ Backtracking/Questions/Subset Sums.java
@@ -1,1 +1,29 @@
+class Solution {
+    // Recursive function to find subset sums
+    public void findSubsetSums(int i, ArrayList<Integer> arr, ArrayList<Integer> ans, int sum) {
+        // Base case: if all elements are considered
+        if (i == arr.size()) {
+            ans.add(sum);
+            return;
+        }
+
+        // Include the current element in the sum and recurse
+        sum += arr.get(i);
+        findSubsetSums(i + 1, arr, ans, sum);
+
+        // Exclude the current element from the sum and recurse
+        sum -= arr.get(i);
+        findSubsetSums(i + 1, arr, ans, sum);
+    }
+
+    // Function to generate subset sums
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+
+        // Start the recursive process from the beginning of the array
+        findSubsetSums(0, arr, ans, 0);
+
+        return ans;
+    }
+}
 
