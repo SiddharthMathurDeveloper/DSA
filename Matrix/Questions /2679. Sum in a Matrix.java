ff9ee32135e8490f39dc63a8f232a481c6b62d1c
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-11-20 10:09:02 +0530

    Update 2679. Sum in a Matrix.java

--- Matrix/Questions /2679. Sum in a Matrix.java
+++ Matrix/Questions /2679. Sum in a Matrix.java
@@ -1,25 +1,34 @@
 class Solution {
+    // Function to calculate the sum of maximum values in each column of a sorted matrix
     public int matrixSum(int[][] grid) {
+        // Get the number of rows and columns in the matrix
         int gridRow = grid.length;
         int gridCol = grid[0].length;
 
-        for(int [] arr: grid){
+        // Sort each row in the matrix in ascending order
+        for (int[] arr : grid) {
             Arrays.sort(arr);
         }
 
+        // Initialize the variable to store the sum of maximum values among rows
+        int sumOfMaxAmongRows = 0;
 
-
-        int sumOfMaxAmongRows=0;
-
-        for(int col=0;col<gridCol; col++){
+        // Iterate through each column
+        for (int col = 0; col < gridCol; col++) {
+            // Initialize the maximum value in the current column
             int maxInColumn = Integer.MIN_VALUE;
-            for(int row=0 ; row<gridRow ; row++){
-                maxInColumn = Math.max( maxInColumn,grid[row][col] );
+
+            // Iterate through each row in the current column
+            for (int row = 0; row < gridRow; row++) {
+                // Update the maximum value in the column
+                maxInColumn = Math.max(maxInColumn, grid[row][col]);
             }
 
-            sumOfMaxAmongRows+=maxInColumn;
+            // Add the maximum value in the current column to the overall sum
+            sumOfMaxAmongRows += maxInColumn;
         }
 
+        // Return the final sum of maximum values among rows
         return sumOfMaxAmongRows;
     }
 }
