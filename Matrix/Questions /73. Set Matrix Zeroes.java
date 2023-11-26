286253bf5f0d81d86bbbb00f8a385ea97f421ac2
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-11-26 11:15:16 +0530

    Update 73. Set Matrix Zeroes.java

--- Matrix/Questions /73. Set Matrix Zeroes.java
+++ Matrix/Questions /73. Set Matrix Zeroes.java
@@ -1,38 +1,46 @@
 class Solution {
+
+    // Function to set zeroes in the matrix
     public void setZeroes(int[][] matrix) {
-        // Get the number of rows and columns in the matrix
-        int matrixRow = matrix.length;
-        int matrixCol = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean firstRow = false;
 
-        // Arrays to mark rows and columns that need to be zeroed
-        int[] rows = new int[matrixRow];
-        int[] cols = new int[matrixCol];
-
-        // Iterate through each element in the matrix
-        for (int i = 0; i < matrixRow; i++) {
-            for (int j = 0; j < matrixCol; j++) {
-                // If the current element is 0, mark the corresponding row and column
+        // Iterate through the matrix to mark cells with value 0 in the first row and column
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == 0) {
-                    if (rows[i] == 0) {
-                        rows[i] = 1; // Mark the row
-                    }
-                    if (cols[j] == 0) {
-                        cols[j] = 1; // Mark the column
+                    matrix[0][j] = 0; // Mark the column in the first row
+                    if (i == 0) {
+                        firstRow = true; // Mark that the first row needs to be zeroed
+                    } else {
+                        matrix[i][0] = 0; // Mark the row in the first column
                     }
                 }
             }
         }
 
-        // Iterate through each element in the matrix again
-        for (int i = 0; i < matrixRow; i++) {
-            for (int j = 0; j < matrixCol; j++) {
-                // If the current element is 0, or its corresponding row or column is marked, set it to 0
-                if (matrix[i][j] == 0 || rows[i] == 1 || cols[j] == 1) {
-                    matrix[i][j] = 0;
+        // Iterate through the matrix (excluding the first row and column) to set zeroes
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0; // Set zeroes based on markings in the first row and column
                 }
             }
         }
 
-        return;
+        // Check if the first element (matrix[0][0]) needs to be zeroed
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0; // Zero out the first column
+            }
+        }
+
+        // Check if the first row needs to be zeroed
+        if (firstRow) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0; // Zero out the first row
+            }
+        }
     }
 }
