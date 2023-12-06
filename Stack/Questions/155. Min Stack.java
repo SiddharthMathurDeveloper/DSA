3ebb2bf185abf831ababfe050b306cfba7ae75f9
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-12-06 19:38:01 +0530

    Update 155. Min Stack.java

--- Stack/Questions/155. Min Stack.java
+++ Stack/Questions/155. Min Stack.java
@@ -55,3 +55,82 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+
+
+/* Solution 2
+
+Time Complexity: O(1)
+Space Complexity: O(N)
+
+*/
+
+
+class MinStack {
+    Stack<Long> stack;   // Stack to store values (using Long for flexibility)
+    Long minValue;       // Variable to track the current minimum value
+
+    public MinStack() {
+        stack = new Stack<>();
+        minValue = Long.MAX_VALUE;  // Initialize the minimum value to the maximum possible Long value
+    }
+    
+    // Function to push a value onto the stack
+    public void push(int val) {
+        Long value = Long.valueOf(val);  // Convert the int value to Long for comparison and storage
+
+        // If the stack is empty, set the minimum value and push the value onto the stack
+        if (stack.isEmpty()) {
+            minValue = value;
+            stack.push(value);
+        } else {
+            // If the new value is smaller than the current minimum, adjust the value before pushing
+            if (minValue > value) {
+                stack.push(2 * value - minValue);
+                minValue = value;
+            } else {
+                stack.push(value);
+            }
+        }
+    }
+    
+    // Function to pop the top element from the stack
+    public void pop() {
+        if (stack.isEmpty()) return;
+
+        Long poppedValue = stack.pop();
+
+        // If the popped value was smaller than the current minimum, adjust the minimum value
+        if (minValue > poppedValue) {
+            minValue = 2 * minValue - poppedValue;
+        }
+    }
+    
+    // Function to get the top element from the stack
+    public int top() {
+        Long peekValue = stack.peek();
+
+        // If the top value is smaller than the current minimum, return the current minimum
+        if (minValue > peekValue) {
+            return minValue.intValue();
+        }
+
+        // Otherwise, return the top value
+        return peekValue.intValue();
+    }
+    
+    // Function to get the minimum element from the stack
+    public int getMin() {
+        return minValue.intValue();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
