3bee17bbd3686208ff3aed1b1ee31324d87f6115
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-12-08 16:08:34 +0530

    Update 946. Validate Stack Sequences.java

--- Stack/Questions/946. Validate Stack Sequences.java
+++ Stack/Questions/946. Validate Stack Sequences.java
@@ -24,3 +24,31 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+
+/* Solution 2
+TC -> O(N)
+SC -> O(1) [Inplace]
+*/
+
+class Solution {
+    // Function to validate stack sequences
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int pushedIndex = 0;  // Index to track elements in the pushed array
+        int poppedIndex = 0;   // Index to track elements in the popped array
+
+        // Iterate through the pushed array
+        for (int value : pushed) {
+            pushed[pushedIndex++] = value;  // Simulate the push operation by updating the pushed array
+
+            // Check if the top element of the pushed array matches the next element to be popped
+            while (pushedIndex > 0 && pushed[pushedIndex - 1] == popped[poppedIndex]) {
+                pushedIndex--;  // If matched, simulate the pop operation by updating pushedIndex
+                poppedIndex++;  // Move to the next element in the popped array
+            }
+        }
+
+        // Check if all elements in the pushed array have been processed, indicating a valid sequence
+        return pushedIndex == 0;
+    }
+}
