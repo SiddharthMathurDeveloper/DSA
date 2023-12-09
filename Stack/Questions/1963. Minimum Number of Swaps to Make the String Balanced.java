266d6f6a36d7571bf989e86767f22894d04a8cdd
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-12-10 03:05:21 +0530

    Update 1963. Minimum Number of Swaps to Make the String Balanced.java

--- Stack/Questions/1963. Minimum Number of Swaps to Make the String Balanced.java
+++ Stack/Questions/1963. Minimum Number of Swaps to Make the String Balanced.java
@@ -33,5 +33,31 @@ class Solution {
 
 /* Solution 2
 TC -> O(N)
-SC -> O(N)
+SC -> O(1)
 */
+
+
+class Solution {
+    public int minSwaps(String s) {
+        int stack_size = 0; // Variable to track the balance of '[' and ']' characters
+
+        // Iterate through the characters in the input string
+        for (int i = 0; i < s.length(); i++) {
+            // If the current character is '[', increment the stack size
+            if (s.charAt(i) == '[') {
+                stack_size++;
+            } else {
+                // If the current character is ']' and the stack is not empty, decrement the stack size
+                if (stack_size > 0) {
+                    stack_size--;
+                }
+            }
+        }
+
+        // The number of swaps needed is half of the remaining stack size (rounded up if odd)
+        return (stack_size + 1) / 2;
+    }
+}
+
+
+
