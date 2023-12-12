155edf7fac1944ab339be8b9b705de7dd005da9f
Siddharth Mathur <133037456+SiddharthMathurDeveloper@users.noreply.github.com>
2023-12-12 17:23:43 +0530

    Update 856. Score of Parentheses.java

--- Stack/Questions/856. Score of Parentheses.java
+++ Stack/Questions/856. Score of Parentheses.java
@@ -22,3 +22,34 @@ class Solution {
         return score;
     }
 }
+
+/* Solution 2
+TC -> O(N)
+SC -> O(N)
+*/
+
+
+class Solution {
+    public int scoreOfParentheses(String s) {
+        int score = 0; // Initialize the total score
+        int depth = 0; // Initialize the current depth of nested parentheses
+
+        char previousParenthese = '('; // Keep track of the previous parenthesis to determine the score
+
+        for (char parenthese : s.toCharArray()) {
+            if (parenthese == '(') {
+                depth++; // Increase the depth for an opening parenthesis
+            } else {
+                depth--; // Decrease the depth for a closing parenthesis
+                if (previousParenthese == '(') {
+                    // If the previous parenthesis was an opening parenthesis, update the score based on the current depth
+                    score += Math.pow(2, depth);
+                }
+            }
+
+            previousParenthese = parenthese; // Update the previous parenthesis
+        }
+
+        return score; // Return the total score
+    }
+}
